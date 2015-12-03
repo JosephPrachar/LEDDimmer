4e88655aca1d86078255029415e391c91ea4bfd7
Thomas Franklin <thomaslfranklin@gmail.com>
2015-12-02 19:50:13 -0800

    Fixed the timer

--- Timer.vhd
+++ Timer.vhd
@@ -26,33 +26,30 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity timer is
     Port ( divided_clk : in STD_LOGIC;
+           reset : in STD_LOGIC;
            max_time : in STD_LOGIC_VECTOR (11 downto 0);
            seconds : out STD_LOGIC_VECTOR (11 downto 0));
 end timer;
 
 architecture Behavioral of timer is
 
-    signal sec,old_max : std_logic_vector (11 downto 0):=x"000";
+    signal sec : std_logic_vector (11 downto 0) := "111111111111";
     
 begin
-
     
-    dec_seconds : process(divided_clk, max_time) begin
-        if NOT (max_time = old_max) then
-            old_max <= max_time;
-            sec <= old_max;
+    dec_seconds : process(reset, divided_clk) begin
+        if RISING_EDGE(reset) then
+            sec <= max_time;
+            seconds <= max_time;
         end if;
         
         if RISING_EDGE(divided_clk) then
             
-            if (sec = x"000") then
-                seconds <= x"1FF";
-            
-            else
+            if NOT (sec = x"000") then
                 sec <= std_logic_vector(unsigned(sec) - 1);
-                seconds <= sec;
-                
             end if;
+            
+            seconds <= sec;
             
         end if;
         
